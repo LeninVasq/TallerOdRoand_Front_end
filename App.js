01e1e5df7cd3dd4f2955136bf79a5a5fad3aafbe
LeninVasq <150496561+LeninVasq@users.noreply.github.com>
2025-05-18 21:22:58 -0600

    primera prueba

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image,Button } from 'react-native';
+import icon from './assets/icon.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={icon}
+        style={{ width: 100, height: 100 }}
+      />
+      <Text style={{color: 'white'}}>Hola!</Text>
+      <StatusBar style="light" />
+      <Button title="pulsa aqui" onPress={() => alert('Hola!')} />
     </View>
   );
 }
@@ -13,7 +19,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#077BFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
